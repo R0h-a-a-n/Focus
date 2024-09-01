4b54e40821a9e6ddc6704c2e57702143c186844c
R0h-a-a-n <yourworstfear178@gmail.com>
2024-09-01 20:18:43 +0530

    Integrate blocking and unblocking of websites with timer functionality

--- blocker.py
+++ blocker.py
@@ -1,4 +1,26 @@
-host1=input("Enter name of website to block:")
-file = open("C:\Windows\System32\drivers\etc\hosts","a")
-file.write(f"127.0.0.1   {host1}")
-file.close()
+def block_websites(websites):
+    with open("C:/Windows/System32/drivers/etc/hosts", "a") as file:
+        for website in websites:
+            file.write(f"127.0.0.1   {website}\n")
+    print("Websites blocked.")
+
+def unblock_websites(websites):
+    # Read the current contents of the hosts file
+    with open("C:/Windows/System32/drivers/etc/hosts", "r") as file:
+        lines = file.readlines()
+
+    # Rewrite the hosts file, excluding the blocked websites
+    with open("C:/Windows/System32/drivers/etc/hosts", "w") as file:
+        for line in lines:
+            if not any(website in line for website in websites):
+                file.write(line)
+    print("Websites unblocked.")
+
+def get_websites_to_block():
+    websites = []
+    while True:
+        website = input("Enter name of website to block (or type 'done' to finish): ")
+        if website.lower() == 'done':
+            break
+        websites.append(website)
+    return websites
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from timer import Timer
+from blocker import block_websites, unblock_websites, get_websites_to_block
 
 def main():
+    websites = get_websites_to_block()
+
+    if not websites:
+        print("No websites to block.")
+        return
+
     timer = Timer()
 
     duration_input = input("Enter the duration for the timer in seconds: ")
@@ -11,12 +18,14 @@ def main():
         if duration > 0:
             def on_start():
                 print("Timer started!")
+                block_websites(websites)
 
             def on_tick(remaining_time):
                 print(f"Time remaining: {remaining_time} seconds")
 
             def on_end():
                 print("Time's up!")
+                unblock_websites(websites)
 
             timer.start_timer(duration, on_timer_start=on_start, on_timer_tick=on_tick, on_timer_end=on_end)
         else:
